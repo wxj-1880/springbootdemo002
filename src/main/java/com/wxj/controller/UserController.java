f085632508ac862a107709163ee0b0730eeab9b6
xiaobai <774474153@qq.com>
2020-08-04 19:09:14 +0800

    小章修改

--- src/main/java/com/wxj/controller/UserController.java
+++ src/main/java/com/wxj/controller/UserController.java
@@ -16,6 +16,7 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserService userService;
+	//小章修改了demo1
 
     @GetMapping("/")
     public String findAll(Model model){
